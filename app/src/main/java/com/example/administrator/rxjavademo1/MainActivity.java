fb916990d838a1a50c337616f2176faca1b5a7aa
gx251zyb <1123221743@qq.com>
2017-03-31 17:55:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/rxjavademo1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.rxjavademo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * 观察者 Observer
+     * 被观察者 Obseverable
+     * 订阅 subscribe
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
